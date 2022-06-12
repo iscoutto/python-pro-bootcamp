cac11d29bf0f82d8852168923559ffd0c82e1926
Isabella <isacoutosoares@gmail.com>
2022-06-12 12:54:25 -0300

    add project day one

---
+++ projects/day-1-project.py
@@ -0,0 +1,6 @@
+# Day 1 Project: Band name generator
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+print("The name of your band is: " + city + " " + pet)
